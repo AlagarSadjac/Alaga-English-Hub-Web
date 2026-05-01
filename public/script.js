70fb501a340ec18a591766d65cb9094321a5b01d
alaga <alagarmca006@gmail.com>
2026-05-01 06:11:04 +0530

    updated v6

--- public/script.js
+++ public/script.js
@@ -167,7 +167,7 @@ function performLogin() {
     const userEmail = document.getElementById('email').value;
     const userPass = document.getElementById('password').value;
 
-    fetch('https://alaga-english-hub-web-api.onrender.com', {
+    fetch('https://alaga-english-hub-web-api.onrender.com/api/student/all', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: userEmail, password: userPass })
