c7ee67ae6075cdffd54e65f895b342e2708be014
alaga <alagarmca006@gmail.com>
2026-05-01 05:53:43 +0530

    Updated v2

--- public/script.js
+++ public/script.js
@@ -63,7 +63,7 @@ function performLogin() {
     const userPass = document.getElementById('password').value;
 
 
-    fetch('http://localhost:8080/api/student/login', {  // ஸ்பிரிங் பூட் API-க்கு தகவலை அனுப்புதல்
+    fetch('https://alaga-english-hub-web-api.onrender.com/api/student/login', {  // ஸ்பிரிங் பூட் API-க்கு தகவலை அனுப்புதல்
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -132,7 +132,7 @@ function performSignUp() {     //  புதிய மாணவர் பதி�
         return;
     }
 
-    fetch('http://localhost:8080/api/student/register', {
+    fetch('https://alaga-english-hub-web-api.onrender.com/api/student/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name: name, email: email, password: pass })
@@ -167,7 +167,7 @@ function performLogin() {
     const userEmail = document.getElementById('email').value;
     const userPass = document.getElementById('password').value;
 
-    fetch('http://localhost:8080/api/student/login', {
+    fetch('https://alaga-english-hub-web-api.onrender.com/api/student/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: userEmail, password: userPass })
